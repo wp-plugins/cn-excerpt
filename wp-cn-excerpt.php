27fee348075e1409d71553f2a16d06726c50d433
joychao.cc <joychao.cc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-11 09:33:59 +0000

    version4.2.8
    git-svn-id: https://plugins.svn.wordpress.org/cn-excerpt/trunk@963622 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-cn-excerpt.php
+++ wp-cn-excerpt.php
@@ -3,7 +3,7 @@
 Plugin Name:WP CN Excerpt
 Plugin URI: http://wordpress.org/plugins/cn-excerpt/
 Description: WordPress高级摘要插件。支持在后台设置摘要长度，摘要最后的显示字符，以及允许哪些html标记在摘要中显示
-Version:4.2.7
+Version:4.2.8
 Author: Joychao
 Author URI: http://weibo.com/joychaocc
 ========================本插件修改自：Advanced excerpt by Bas van Doren(http://basvd.com/)==========================
